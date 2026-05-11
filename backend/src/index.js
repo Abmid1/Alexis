3920b04225ef6538b79ba1854f04e4e80778df2c
abmid1 <tipagya518@gmail.com>
2026-05-11 19:42:06 +0000

    vercel url added

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 // ── CORS ──────────────────────────────────────────────────────────
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:5000'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:5000','https://alexis-git-main-tipagya518-3934s-projects.vercel.app'],
   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
